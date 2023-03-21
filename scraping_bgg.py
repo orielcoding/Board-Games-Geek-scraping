43b3ccf1e3898a48174c92956588b7a386879327
Maria Blinchevskaya <m.blinchevskaya@gmail.com>
2023-03-21 23:29:08 +0200

    added get_title and get_stats methods

--- scraping_bgg.py
+++ scraping_bgg.py
@@ -27,78 +27,112 @@ def get_urls(page_num: int) -> list[str]:
 
 
 class Game:
-    def __init__(self, index, url: str, driver):
-        driver.get(url)
-        driver.implicitly_wait(20)
-        self.index = index
-        self.html = BeautifulSoup(driver.execute_script("return document.body.outerHTML;"), "lxml")
+    def __init__(self, url: str, driver):
+        self.html: BeautifulSoup = self.get_html(url, driver)
+        self.html_stats: BeautifulSoup = self.get_html(f"{url}/stats", driver)
         self.gameplay_panel = self.html.find(class_="panel panel-bottom ng-scope")
         self.credits_panel = self.html.find(class_="credits ng-scope")
         self.features_panel = self.html.find(class_="panel panel-bottom game-classification ng-scope")
-        self.title_panel = self.html.find(class_="game-header-title-info")
-        self.identifiers: list = self.get_gameplay() #+ self.get_features() + self.get_creators()
-        self._title = self.get_title()
-        print(self._title)
+        self.info: dict = self.get_title() | self.get_gameplay() | self.get_features() | self.get_creators() | self.get_stats()
 
-    def get_title(self):
-        """
-        Returns title of the game
-        """
-        results_title = self.html.find(class_='expandable-body')
-        results_text = results_title.text.strip().split()
-        index = results_text.index('is')
-        title = ''
-        for i in range(index):
-            title += f'{results_text[i]} '
-        return title
+    def get_html(self, url: str, driver):
+        driver.get(url)
+        return BeautifulSoup(driver.execute_script("return document.body.outerHTML;"), "lxml")
 
-    def get_gameplay(self) -> list:
+    def get_title(self) -> dict:
         """
-        Returns list containing: num players, time duration, age limit, weight (complexity of the game)
+        Returns dictionary containing game title and release year
+        """
+        html = self.html.find_all(class_="game-header-title-info")
+        text_list = html[1].text.strip().split()
+        game_title = ""
+        for item in text_list:
+            if item[0] == '(':
+                game_year = int(item[1:-1])
+                break
+            else:
+                game_title += f"{item} "
+
+        return {"game_title": game_title, "game_year": game_year}
+
+    def get_gameplay(self) -> dict:
+        """
+        Returns dictionary containing: num players, time duration, age limit, weight (complexity of the game)
         """
         gameplay_items = self.gameplay_panel.find_all(class_="gameplay-item")
-        num_players_pattern = gameplay_items[0].find(class_="ng-scope ng-isolate-scope").find_all(
-            class_="ng-binding ng-scope")
-        min_players: int = int(re.search(r">[1-9]<", str(num_players_pattern[0])).group()[1])
-        max_players: int = int(re.search(r">[1-9]<", str(num_players_pattern[1])).group()[1])
-        num_players: tuple[int, int] = (min_players, max_players)
 
-        time_pattern = gameplay_items[1].find_all(class_="ng-binding ng-scope")
-        min_time: int = int(re.search(r">[0-9]*<", str(time_pattern[0])).group()[1:-1])
-        max_time: int = int(re.search(r">([0-9]+)<", str(time_pattern[1])).group()[1:-1])
-        time_duration: tuple[int, int] = (min_time, max_time)
+        num_pattern = gameplay_items[0].find_all('span', class_= 'ng-binding ng-scope')
+        if len(num_pattern)>1:
+            num_players = tuple([int(num_pattern[0].text.strip()), int(num_pattern[1].text.strip()[1:])])
+        else:
+            num_players = tuple([int(num_pattern[0].text.strip())])
 
-        age_boundary: int = int(re.search(r"[0-5]*(?=\+)", str(gameplay_items[2])).group())
+        time_pattern = gameplay_items[1].find_all('span', class_= 'ng-binding ng-scope')
 
-        weight: float = float(re.search(r"[0-5]\.[0-9]{2}", str(gameplay_items[3])).group())
-        return [num_players, time_duration, age_boundary, weight]
+        if len(time_pattern)>1:
+            time_duration = tuple([int(time_pattern[0].text.strip()), int(time_pattern[1].text.strip()[1:])])
+        else:
+            time_duration = tuple([int(time_pattern[0].text.strip())])
 
-    def get_features(self) -> list:
+        # age_limit: int = int(re.search(r"[0-5]*(?=\+)", str(gameplay_items[2])).group())
+        age_limit = 0
+        # weight: float = float(re.search(r"[0-5]\.[0-9]{2}", str(gameplay_items[3])).group())
+        weight = 0
+        return {"num_players": num_players, "time_duration": time_duration, "age_limit": age_limit, "weight": weight}
+
+    def get_features(self) -> dict:
         """
-        Returns list containing: type, category, mechanism
+        Returns dictionary containing: type, category, mechanism
         """
-        pass
+        features_items = self.features_panel.find_all(class_="feature ng-scope")
 
-    def get_creators(self) -> list:
-        """
-        Returns list containing: designers and artists
-        """
-        pass
+        game_type: str = features_items[0].find('a', {'class': 'ng-binding'}).string
 
-    def get_info(self):
+        category: tuple = tuple([i.get('title') for i in features_items[1].find_all('a', {'class': 'ng-binding'})[:-1]])
+
+        mechanism: tuple = tuple([i.get('title') for i in features_items[2].find_all('a', {'class': 'ng-binding'})[:-1]])
+
+        reimplements = tuple([i.get('title') for i in features_items[-1].find_all('a', {'class': 'ng-binding'})[:-1]])
+
+        return {"game_type": game_type, "category": category, "mechanism": mechanism, "reimplements": reimplements}
+
+    def get_creators(self) -> dict:
+        """
+        Returns dictionary containing: designers and artists
+        """
+        creators_items = self.credits_panel.find_all(action="geekitemctrl.showFullCredits")
+
+        designers_line = creators_items[0].find_all(attrs={"title": True})
+        designers: tuple = tuple([designer.get('title') for designer in designers_line])
+
+        artists_line = creators_items[1].find_all(attrs={"title": True})
+        artists: tuple = tuple([artist.get('title') for artist in artists_line])
+
+        return {"designers": designers, "artists": artists}
+
+    def get_stats(self) -> dict:
+        """
+        Returns dictionary containing statistical info about the games
+        """
+        html = self.html_stats.find_all(class_="outline-item-description")
+        aggregate_rating = html[0].text.strip()
+        review_count = html[1].text.strip()
+        num_comments = html[4].text.strip()
+        page_views = html[6].text.strip()
+        all_time_plays = html[9].text.strip()
+        this_month_plays = html[10].text.strip()
+        num_own = html[11].text.strip()
+        num_wishlist = html[15].text.strip()
+
+        return {"aggregate_rating": aggregate_rating, "review_count": review_count, "num_comments": num_comments,
+                "page_views": page_views, "all_time_plays": all_time_plays, "this_month_plays": this_month_plays,
+                "num_own": num_own, "num_wishlist": num_wishlist}
+
+    def get_info(self) -> dict:
         """
         Returns all the info of a game
         """
-        pass
-
-    def __str__(self):
-        """
-        returns string representation of an instance
-        :return:
-        """
-        return(f'{self.index} - {self._title} > \n   Num_players(from,to): {self.identifiers[0]}, '
-               f'time_duration(from,to): {self.identifiers[1]}, age_boundary: > {self.identifiers[2]}, '
-               f'weight: {self.identifiers[3]}')
+        return self.info
 
 
 def main():
@@ -113,11 +147,10 @@ def main():
     for index in range(2):
         games_pages_urls += get_urls(index)
 
-    games: dict = {} # building a dictionary of Game-objects
-    for index, url in enumerate(games_pages_urls[:1]):
-        games[f"game_{index}"] = Game(index, url, driver)
-        print(games[f"game_{index}"])
-    driver.quit()
+    games: dict = {}
+    for index, url in enumerate(games_pages_urls[52:100]):
+        games[f"game_{index}"]: Game = Game(url, driver)
+        print(games[f"game_{index}"].get_info())
 
 
 if __name__ == "__main__":
