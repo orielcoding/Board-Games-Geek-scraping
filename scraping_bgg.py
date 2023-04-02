8041fcd66253067e2e42360b533423d767f13e40
Oriel Singer <oriel2486@gmail.com>
2023-04-02 14:25:26 +0300

    solved problems with the scraping pattern of the age limit and the title. also, added scraping for general and types ranking.

--- scraping_bgg.py
+++ scraping_bgg.py
@@ -1,11 +1,8 @@
-import grequests
 import requests
 from selenium import webdriver
 from selenium.webdriver import Chrome
 from bs4 import BeautifulSoup
 import json
-import time
-import logging
 import re
 
 with open("BGG_configuration.json", "r") as f:
@@ -26,18 +23,20 @@ def get_urls(page_num: int) -> list[str]:
     return [config["DOMAIN"] + tag.find(attrs={"href": True}).get("href") for tag in tags]
 
 
+def get_html(url: str, driver):
+    driver.get(url)
+    return BeautifulSoup(driver.execute_script("return document.body.outerHTML;"), "lxml")
+
+
 class Game:
     def __init__(self, url: str, driver):
-        self.html: BeautifulSoup = self.get_html(url, driver)
-        self.html_stats: BeautifulSoup = self.get_html(f"{url}/stats", driver)
+        self.html: BeautifulSoup = get_html(url, driver)
+        self.html_stats: BeautifulSoup = get_html(f"{url}/stats", driver)
         self.gameplay_panel = self.html.find(class_="panel panel-bottom ng-scope")
         self.credits_panel = self.html.find(class_="credits ng-scope")
         self.features_panel = self.html.find(class_="panel panel-bottom game-classification ng-scope")
-        self.info: dict = self.get_title() | self.get_gameplay() | self.get_features() | self.get_creators() | self.get_stats()
-
-    def get_html(self, url: str, driver):
-        driver.get(url)
-        return BeautifulSoup(driver.execute_script("return document.body.outerHTML;"), "lxml")
+        self.features = self.get_features()
+        self.info: dict = self.get_title() | self.get_gameplay() | self.features | self.get_creators() | self.get_stats()
 
     def get_title(self) -> dict:
         """
@@ -45,9 +44,10 @@ class Game:
         """
         html = self.html.find_all(class_="game-header-title-info")
         text_list = html[1].text.strip().split()
-        game_title = ""
+        game_title: str = ""
+        game_year: int = None
         for item in text_list:
-            if item[0] == '(':
+            if item[0] == '(' and item[5] == ')':
                 game_year = int(item[1:-1])
                 break
             else:
@@ -61,23 +61,23 @@ class Game:
         """
         gameplay_items = self.gameplay_panel.find_all(class_="gameplay-item")
 
-        num_pattern = gameplay_items[0].find_all('span', class_= 'ng-binding ng-scope')
-        if len(num_pattern)>1:
+        num_pattern = gameplay_items[0].find_all('span', class_='ng-binding ng-scope')
+        if len(num_pattern) > 1:
             num_players = tuple([int(num_pattern[0].text.strip()), int(num_pattern[1].text.strip()[1:])])
         else:
             num_players = tuple([int(num_pattern[0].text.strip())])
 
-        time_pattern = gameplay_items[1].find_all('span', class_= 'ng-binding ng-scope')
+        time_pattern = gameplay_items[1].find_all('span', class_='ng-binding ng-scope')
 
-        if len(time_pattern)>1:
+        if len(time_pattern) > 1:
             time_duration = tuple([int(time_pattern[0].text.strip()), int(time_pattern[1].text.strip()[1:])])
         else:
             time_duration = tuple([int(time_pattern[0].text.strip())])
 
-        # age_limit: int = int(re.search(r"[0-5]*(?=\+)", str(gameplay_items[2])).group())
-        age_limit = 0
-        # weight: float = float(re.search(r"[0-5]\.[0-9]{2}", str(gameplay_items[3])).group())
-        weight = 0
+        age_limit: int = int(re.search(r"[0-9]*(?=\+)", str(gameplay_items[2])).group())
+        # age_limit = 0
+        weight: float = float(re.search(r"[0-5]\.[0-9]{2}", str(gameplay_items[3])).group())
+        # weight = 0
         return {"num_players": num_players, "time_duration": time_duration, "age_limit": age_limit, "weight": weight}
 
     def get_features(self) -> dict:
@@ -86,11 +86,12 @@ class Game:
         """
         features_items = self.features_panel.find_all(class_="feature ng-scope")
 
-        game_type: str = features_items[0].find('a', {'class': 'ng-binding'}).string
+        game_type: tuple = tuple(i.string for i in features_items[0].find_all('a', {'class': 'ng-binding'}))
 
         category: tuple = tuple([i.get('title') for i in features_items[1].find_all('a', {'class': 'ng-binding'})[:-1]])
 
-        mechanism: tuple = tuple([i.get('title') for i in features_items[2].find_all('a', {'class': 'ng-binding'})[:-1]])
+        mechanism: tuple = tuple(
+            [i.get('title') for i in features_items[2].find_all('a', {'class': 'ng-binding'})[:-1]])
 
         reimplements = tuple([i.get('title') for i in features_items[-1].find_all('a', {'class': 'ng-binding'})[:-1]])
 
@@ -112,20 +113,27 @@ class Game:
 
     def get_stats(self) -> dict:
         """
-        Returns dictionary containing statistical info about the games
+        Returns dictionary containing statistical info about the games.
         """
         html = self.html_stats.find_all(class_="outline-item-description")
         aggregate_rating = html[0].text.strip()
         review_count = html[1].text.strip()
         num_comments = html[4].text.strip()
         page_views = html[6].text.strip()
-        all_time_plays = html[9].text.strip()
-        this_month_plays = html[10].text.strip()
-        num_own = html[11].text.strip()
-        num_wishlist = html[15].text.strip()
+        overall_rank = int(re.match('[0-9]+', html[7].text.strip()).group())
+
+        num_types: int = len(self.features["game_type"])
+        types_rank: list = []
+        for index in range(num_types):
+            types_rank.append(int(re.match('[0-9]+', html[8 + index].text.strip()).group()))
+        all_time_plays = int(html[9 + num_types - 1].text.strip())
+        this_month_plays = int(html[10 + num_types - 1].text.strip())
+        num_own = int(html[11 + num_types - 1].text.strip())
+        num_wishlist = int(html[15 + num_types - 1].text.strip())
 
         return {"aggregate_rating": aggregate_rating, "review_count": review_count, "num_comments": num_comments,
-                "page_views": page_views, "all_time_plays": all_time_plays, "this_month_plays": this_month_plays,
+                "page_views": page_views, "overall_rank": overall_rank, "all_time_plays": all_time_plays,
+                "this_month_plays": this_month_plays,
                 "num_own": num_own, "num_wishlist": num_wishlist}
 
     def get_info(self) -> dict:
@@ -142,15 +150,14 @@ def main():
     driver = Chrome(options=options)
     driver.implicitly_wait(5)
 
-    games_pages_urls: list[str] = []
-    # for index in range(config["NUM_GAMES_TO_COLLECT"] // config["NUM_GAMES_PER_PAGE"]):
-    for index in range(2):
-        games_pages_urls += get_urls(index)
+    games_pages_urls: list = []
+    for index in range(config["NUM_GAMES_TO_COLLECT"] // config["NUM_GAMES_PER_PAGE"]):
+        games_pages_urls.append(get_urls(index))
 
     games: dict = {}
-    for index, url in enumerate(games_pages_urls[52:100]):
-        games[f"game_{index}"]: Game = Game(url, driver)
-        print(games[f"game_{index}"].get_info())
+    for list_index, lst in enumerate(games_pages_urls):
+        for index, url in enumerate(lst):
+            games[f"game_{list_index * 100 + index}"]: Game = Game(url, driver)
 
 
 if __name__ == "__main__":
