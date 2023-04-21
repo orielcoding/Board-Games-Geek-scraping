0347cdec74725c1742a833df0a1c5ae20cab3cad
Maria Blinchevskaya <m.blinchevskaya@gmail.com>
2023-04-21 18:07:54 +0300

    added functionality of retreaving BGA game_id and moved global parameners to confuguration file

---
+++ BGA_API_request.py
@@ -0,0 +1,64 @@
+from urllib.request import urlopen
+import json
+import re
+
+with open("BGG_configuration.json", "r") as f:
+    config = json.load(f)
+
+url_base_api = config["URL_BASE_API"]
+api_client_id = config["API_CLIENT_ID"]
+
+game_name_list = ['Brass: Birmingham', 'Pandemic Legacy: Season 1', 'Gloomhaven'] # for tuning
+
+
+def get_game_bga_id(game_name):
+    """
+    Receives game_name and returns game_id from "Board Game Atlas" (API) site
+    """
+    url_game = f'{url_base_api}search?name={game_name}&client_id={api_client_id}'
+
+    with urlopen(url_game) as url:
+        data = json.load(url)
+
+    return data["games"][0]["id"]
+
+
+def get_prices_api(game_name):
+    """
+    Gets the game prices in all the US board game shops.
+    Checks if the game is currently available in the shop and
+    returns the list of dictionaries with each dictionary representing one
+    board game shop with the game available and containing info on:
+    store name, game price and the time it was last updated
+    """
+    game_id = get_game_bga_id(game_name)
+    url = f'{url_base_api}game/prices?game_id={game_id}&client_id={api_client_id}'
+    with urlopen(url) as url:
+        data = json.load(url)
+
+    results: list = []
+    counter = 1
+
+    for item in data["gameWithPrices"]["us"]:
+        if item["in_stock"] is True:
+            store_dict: dict = {"store_name": item["store_name"],
+                                "price": item["price"],
+                                "updated": item["updated_at_ago"]}
+            results.append(store_dict)
+            counter += 1
+
+    return results
+
+
+for game in game_name_list:
+    space = ' '
+    if space in game:
+        game_split = re.split(r' |:', game)
+        game_no_space = ''
+        for word in game_split:
+            if len(word)>0: game_no_space += f'{word}-'
+    else:
+        game_no_space = game
+
+    print(get_prices_api(game_no_space))
+
